9f3ff03aaa6b983edba95c9a182fbd2422c2cd53
Ryan <r.mckee@qub.ac.uk>
2025-02-18 17:58:03 +0000

    update

--- colav_unsafe_set_gen/unsafe_set/__init__.py
+++ colav_unsafe_set_gen/unsafe_set/__init__.py
@@ -1,4 +1,4 @@
-from colav_unsafe_set_gen.risk_assesment import *
+from colav_unsafe_set_gen.risk_assessment import *
 from colav_unsafe_set_gen import objects
 from .unsafe_set import create_unsafe_set_polyshape
 
